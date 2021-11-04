37bb8e5e4d72ad002ce8e7392b0fe193bccbe0bc
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-04 21:38:10 +0200

    WallService fixed

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,43 +1,24 @@
 object WallService  {
     private var posts = emptyArray<Post>()
-    private var id: Int = 0
+    private var lastId: Int = 1
+    val arrForTest = posts.clone()
 
     fun addPost(post: Post): Post {
-        id +=1
-        post.postId = id
-        posts += post.copy()
+        val newPost = post.copy(postId = lastId)
+        posts += newPost
+        lastId ++
         return posts.last()
     }
 
     fun update(post: Post):Boolean {
-        var modId = post.component1()
-        var postToUpdate = post
-        posts.forEachIndexed { index, post ->
-            if (modId == post.id) {
-                posts[index] = post.copy(
-                    fromId = postToUpdate.fromId,
-                    createdBy = postToUpdate.createdBy,
-                    text = postToUpdate.text,
-                    replyOwnerId = postToUpdate.replyOwnerId,
-                    replyPostId = postToUpdate.replyPostId,
-                    friendsOnly = postToUpdate.friendsOnly,
-                    comments = postToUpdate.comments,
-                    copyright = postToUpdate.copyright,
-                    likes = postToUpdate.likes,
-                    reposts = postToUpdate.reposts,
-                    views = postToUpdate.views,
-                    postType = postToUpdate.postType,
-                    singerId = postToUpdate.singerId,
-                    canPin = postToUpdate.canPin,
-                    canDelete = postToUpdate.canDelete,
-                    isPinned = postToUpdate.isPinned,
-                    markedAsAds = postToUpdate.markedAsAds,
-                    isFavorite = postToUpdate.isFavorite,
-                    donut = postToUpdate.donut
-                )
-                return  true
+        var isPostInArray = false
+        for((idex,oldPost) in posts.withIndex()){
+            if(post.id === oldPost.id) {
+                posts[idex]= post.copy(ownerId = oldPost.ownerId, date = oldPost.date)
+                isPostInArray = true
             }
         }
-        return false
+        return isPostInArray
     }
+
 }
