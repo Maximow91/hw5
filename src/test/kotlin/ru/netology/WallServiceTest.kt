3f479da8d1a06eb18298e82c76e6fd57169b4e94
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-04 21:39:53 +0200

    WallServiceTest added

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,16 +1,73 @@
 package ru.netology
 
+import Post
+import WallService.addPost
+import WallService.arrForTest
+import WallService.update
 import org.junit.Test
 
 import org.junit.Assert.*
 
 class WallServiceTest {
+    val testPost = Post(1,12,15,232,1,
+        "Text",1,2,true,12,
+        1,1,1,1,"type",1,
+        true,true,true,true,
+        true,true,1,1)
+
+    val testPostSecond = Post(2,2,2,2,2,
+    "text",2,2,true,2,
+        2,2,2,2,"type",2,
+    true,true,true,true,
+    true,true,2,2)
+
+    val testPostThird = Post (3,3,3,3,3,
+    "text",3,3,true,3,
+    3,3,3,3,"type",3,
+    true,true,true,true,
+    true,true,3,3)
+
+    val testPostWithoutExistingId = Post(99,1,1,1,12,
+        "text",7, 7,true,7,
+        7,7,7,7,"type", 7,
+        true,true,true,true,
+        true,true,7,7 )
+
+    val testPostToUpdate = Post(1,8,8,8,8,
+    "text",8,8,true,8,
+    8,8,8,8,"type",8,
+    true,true,true,true,
+    true,true,8,8)
+
 
     @Test
-    fun addPost() {
+    fun addPostTest () {
+        val result = addPost(testPost)
+        val testRes = addPost(testPostSecond)
+        assertNotEquals(result.postId, 0)
+
     }
 
     @Test
-    fun update() {
+    fun testUpdate() {
+        val testArr = arrForTest.toMutableList()
+        testArr.clear()
+        testArr.toTypedArray()
+        addPost(testPost)
+        addPost(testPost)
+        addPost(testPostSecond)
+        addPost(testPostThird)
+
+        val noUpdate = update(testPostWithoutExistingId)
+
+        assertEquals(noUpdate, false)
+
+        val up = update(testPostToUpdate)
+
+        assertEquals(up, true)
+
+
+
+
     }
 }
