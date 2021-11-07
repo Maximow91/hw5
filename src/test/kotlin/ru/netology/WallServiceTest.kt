37d8c94449198176c743b2f08f4bb5cb80ccaf02
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-07 22:34:30 +0200

    WallServiceTest modified

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -7,37 +7,39 @@ import WallService.update
 import org.junit.Test
 
 import org.junit.Assert.*
+import post.properties.Attachments
+import post.properties.VideoAttachments
 
 class WallServiceTest {
     val testPost = Post(1,12,15,232,1,
         "Text",1,2,true,12,
         1,1,1,1,"type",1,
         true,true,true,true,
-        true,true,1,1,null,null,null)
+        true,true,1,1,null,null,null,VideoAttachments(1,2,"video","video"))
 
     val testPostSecond = Post(2,2,2,2,2,
     "text",2,2,true,2,
         2,2,2,2,"type",2,
     true,true,true,true,
-    true,true,2,2,null,null,null)
+    true,true,2,2,null,null,null,VideoAttachments(1,2,"video","video"))
 
     val testPostThird = Post (3,3,3,3,3,
     "text",3,3,true,3,
     3,3,3,3,"type",3,
     true,true,true,true,
-    true,true,3,3,null,null,null)
+    true,true,3,3,null,null,null,VideoAttachments(1,2,"video","video"))
 
     val testPostWithoutExistingId = Post(99,1,1,1,12,
         "text",7, 7,true,7,
         7,7,7,7,"type", 7,
         true,true,true,true,
-        true,true,7,7,null,null,null )
+        true,true,7,7,null,null,null,VideoAttachments(1,2,"video","video") )
 
     val testPostToUpdate = Post(1,8,8,8,8,
     "text",8,8,true,8,
     8,8,8,8,"type",8,
     true,true,true,true,
-    true,true,8,8,null,null,null)
+    true,true,8,8,null,null,null,VideoAttachments(1,2,"video","video"))
 
 
     @Test
