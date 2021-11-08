60cd6a08e29af896ae3e58909f445184dbca9423
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-08 17:40:22 +0200

    audio added

--- src/main/kotlin/post/properties/attachments/data/Audio.kt
+++ src/main/kotlin/post/properties/attachments/data/Audio.kt
@@ -2,7 +2,9 @@ package post.properties.attachments.data
 
 import post.properties.attachments.interfaces.AudioAttachment
 
-data class Audio(override val type: String ="audio",
+data class Audio(
                  override val name: String,
                  override val author: String,
-                 override val bitRate: Int, override val typeAttach: AudioAttachment ):AudioAttachment
+                 override val bitRate: Int,
+                 override val type: String ="audio",
+                 ):AudioAttachment
