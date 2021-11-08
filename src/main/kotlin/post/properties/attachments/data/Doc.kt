9eadeca8cebad87ad2d0bfe4da64849393899bea
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-08 17:59:23 +0200

    Doc added

--- src/main/kotlin/post/properties/attachments/data/Doc.kt
+++ src/main/kotlin/post/properties/attachments/data/Doc.kt
@@ -2,7 +2,8 @@ package post.properties.attachments.data
 
 import post.properties.attachments.interfaces.DocAttachment
 
-data class Doc(override val id: Int,
+data class Doc(val doc: Any?,
+               override val id: Int,
                override val format: String,
                override val content: String,
                override val type: String = "doc"
