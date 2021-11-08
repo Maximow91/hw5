fb1233014f19588be380faf14962af14e113ee73
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-08 17:42:19 +0200

    photo added

--- src/main/kotlin/post/properties/attachments/data/Photo.kt
+++ src/main/kotlin/post/properties/attachments/data/Photo.kt
@@ -2,8 +2,8 @@ package post.properties.attachments.data
 
 import post.properties.attachments.interfaces.PhotoAttachment
 
-data class Photo(override val type: String = "photo",
-                 override val id: Int,
+data class Photo(override val id: Int,
                  override val ownerId: Int,
                  override val photoPreview: String,
-                 override val fullSizePhoto: String): PhotoAttachment
+                 override val fullSizePhoto: String,
+                 override val type: String = "photo"): PhotoAttachment
