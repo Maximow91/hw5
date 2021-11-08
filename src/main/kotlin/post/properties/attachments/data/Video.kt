af1c9c76f15e22a3d197eb71e320195cec4a7fb7
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-08 17:41:04 +0200

    video added

--- src/main/kotlin/post/properties/attachments/data/Video.kt
+++ src/main/kotlin/post/properties/attachments/data/Video.kt
@@ -3,8 +3,8 @@ package post.properties.attachments.data
 import post.properties.attachments.interfaces.Attachments
 import post.properties.attachments.interfaces.VideoAttachment
 
-data class Video (override val type:String = "video",
-                  override val id: Int,
+data class Video (override val id: Int,
                   override val ownerId: Int,
                   override val fullSizeVideo: String,
-                  override val videoPreview: String ):VideoAttachment
+                  override val videoPreview: String,
+                  override val type:String = "video"):VideoAttachment
