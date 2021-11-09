e70da1e584ba9b2d0faa3bbcd4a0c511129a0ac0
Kirill Maximow <markizzzzka91@gmail.com>
2021-11-09 21:50:01 +0200

    WallService modified

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,24 +1,39 @@
 package ru.netology
 
+import Comment
+import CommentNotFoundException
 import Post
+import PostNotFoundException
+import Report
 import WallService.addPost
-import WallService.arrForTest
+import WallService.createComment
 import WallService.update
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
 import post.properties.attachments.data.Audio
+import post.properties.attachments.data.Video
 import post.properties.attachments.interfaces.Attachments
 import post.properties.attachments.interfaces.AudioAttachment
 import post.properties.attachments.interfaces.VideoAttachment
+import kotlin.test.AfterTest
 
 class WallServiceTest {
-    val testPost = Post(1,12,15,232,1,
-        "Text",1,2,true,12,
+
+
+    val attachmentsForTest = Video("Video",1,2,"fullSize","preview")
+    val reportForTest = Report(1,1,0)
+    val reportForTestSecond = Report(2,2,0)
+    val commentForTest = Comment(1,2,1,1,"comment",1,2,3,attachmentsForTest,null,null)
+    val commentForExceptionTest = Comment(1,2,100,1,"comment",1,2,3,attachmentsForTest,null,null)
+    val testPost = Post(8,12,15,232,1,
+        "Text",1,2,true,null,
         1,1,1,1,"type",1,
         true,true,true,true,
         true,true,1,1,null,null,null,Audio(null,"song","vasia",128))
-    val testPostSecond = Post(2,2,2,2,2,
+
+    val testPostSecond = Post(1,2,2,2,2,
     "text",2,2,true,2,
         2,2,2,2,"type",2,
     true,true,true,true,
@@ -48,15 +63,12 @@ class WallServiceTest {
         val result = addPost(testPost)
         val testRes = addPost(testPostSecond)
         assertNotEquals(result.postId, 0)
-
     }
+
 
     @Test
     fun testUpdate() {
-        val testArr = arrForTest.toMutableList()
-        testArr.clear()
-        testArr.toTypedArray()
-        addPost(testPost)
+        WallService.deleteArray()
         addPost(testPost)
         addPost(testPostSecond)
         addPost(testPostThird)
@@ -68,9 +80,42 @@ class WallServiceTest {
         val up = update(testPostToUpdate)
 
         assertEquals(up, true)
-
-
-
-
     }
+
+    @Test
+    fun testCreateComment() {
+
+        val res = WallService.createComment(commentForTest)
+        assertEquals(res,true)
+    }
+
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        WallService.deleteArray()
+        WallService.addPost(testPostSecond)
+        createComment(commentForExceptionTest)
+    }
+
+    @Test
+    fun reportCommentTest(){
+        WallService.addPost(testPost)
+        WallService.addPost(testPostSecond)
+        WallService.createComment(commentForTest)
+        val res = WallService.reportComment(reportForTest)
+        assertEquals(res,true)
+    }
+
+    @Test(expected = CommentNotFoundException::class)
+    fun shouldThrowCommentNotFound() {
+        WallService.reportComment(reportForTestSecond)
+    }
+    @After
+    fun returnArray(){
+        WallService.returnArray()
+    }
+
+
+
+
 }
